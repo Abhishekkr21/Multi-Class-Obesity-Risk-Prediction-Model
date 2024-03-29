aa34957099d95bfedaf517f5a991fb21214e4c69
Abhishek Kumar <104790457+Abhishekkr21@users.noreply.github.com>
2024-03-29 19:15:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-t=int (input())
-for i in range(t):
-    n= int(input())
-    s=input().strip()
 
-    k=s
-    s[::-1] 
-
-    
-    if s>=k:
-        print(k)
-    else : 
-        print(s+k)
-                 
 
     
 
