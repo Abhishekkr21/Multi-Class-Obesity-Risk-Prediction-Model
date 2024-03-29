b409d8593c2aee06b1b1de4917b4d6cffb2881d0
Abhishek Kumar <104790457+Abhishekkr21@users.noreply.github.com>
2024-03-29 19:09:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+t=int (input())
+for i in range(t):
+    n= int(input())
+    s=input().strip()
+
+    k=s
+    s[::-1] 
+
+    
+    if s>=k:
+        print(k)
+    else : 
+        print(s+k)
+                 
+
+    
+
+    
